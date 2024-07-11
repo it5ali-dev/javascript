a99e41e82bd69639f632f1c85a8d890f6335c4a6
Ali Hassan <160418871+it5ali-dev@users.noreply.github.com>
2024-07-11 22:48:21 +0500

    starts with basics

--- basics/first.js
+++ basics/first.js
@@ -1,2 +1,76 @@
+/*
 let name = "ali Hassan"
-console.log(name);
+if (name.includes('ali')) {
+    alert("Hello Ali !")
+} else {
+    alert("Hello Guest !")
+}
+console.log(40 + 23 + 8 - 10);
+
+let country = "Pakistan"
+let continent = "Asia"
+let population = 27.56
+
+console.log(country, continent, population);
+*/
+
+/*
+let fun = true
+fun = 'true'
+console.log(typeof fun);
+
+let year
+
+console.log(year);
+console.log(typeof year);
+
+year = 2002
+console.log(year);
+console.log(typeof year);
+console.log(typeof null);
+*/
+
+/*
+// comparison operator
+let ageAli = 22
+let ageAyesha = 21
+console.log(ageAli > ageAyesha);
+console.log(ageAyesha >= 18);
+
+let isFullAge = ageAyesha >= 18
+console.log(isFullAge);
+
+let now = Date.now()
+console.log(now - 1991 < now - 2024);
+*/
+
+/*
+// Challange No. 1
+// for Mark
+console.log("For Mark");
+let mass = 95
+let height = 1.88
+
+let markBMI = mass / (height * height)
+console.log(`Mark's BMI : ${markBMI.toFixed(2)}`);
+
+console.log("For John");
+mass = 85
+height = 1.76
+
+let johnBMI = mass / (height * height)
+console.log(`John's BMI : ${johnBMI.toFixed(2)}`);
+
+let markHeightBMI = false
+
+if (markBMI > johnBMI) markHeightBMI = true
+console.log(`Mark has greater BMI than John: ${markHeightBMI}`);
+*/
+
+const firstName = "Ali"
+const job = "Software Eng"
+const BirthYear = 2002
+const year = 2024
+
+const ali = "I'm " + firstName + ', a ' + (year - BirthYear) + ' years old ' + job + '!'
+console.log(ali);
