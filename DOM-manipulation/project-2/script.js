a8a0837dbb37d638ed47fc277d86db1c4c709b67
Ali Hassan <160418871+it5ali-dev@users.noreply.github.com>
2024-07-19 09:52:32 +0500

    started the modal window project

---
+++ DOM-manipulation/project-2/script.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const closeModal = document.querySelector('.close-modal')
+const showModal = document.querySelectorAll('.show-modal')
+
+const close = function () {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+const open = function () {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+for (let i = 0; i < showModal.length; i++) {
+    showModal[i].addEventListener('click', open)
+}
+
+closeModal.addEventListener('click', close)
+
+overlay.addEventListener('click', close)
