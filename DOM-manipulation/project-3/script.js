4def2df7207806920986e1abf1d632e69c41f231
Ali Hassan <160418871+it5ali-dev@users.noreply.github.com>
2024-07-20 23:25:47 +0500

    complete and modify the design a bit and also add some js

--- DOM-manipulation/project-3/script.js
+++ DOM-manipulation/project-3/script.js
@@ -1,2 +1,12 @@
 'use strict'
 
+const score1 = document.querySelector('.score-1')
+const score2 = document.querySelector('.score-2')
+const imgs = document.querySelectorAll('.img')
+
+score1.textContent = 0
+score2.textContent = 0
+
+for (let i = 0; i < imgs.length; i++) {
+    imgs[i].classList.add('hidden')
+}
