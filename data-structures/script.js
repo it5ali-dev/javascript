9e0109bff10de2e4f83c81f3a040c424980f4d02
Ali Hassan <160418871+it5ali-dev@users.noreply.github.com>
2024-07-29 23:52:45 +0500

    learn arrays and objects destructuring in depth

---
+++ data-structures/script.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegeterian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    order: function (startIndex, mainIndex) { }
+}
+
+// Object destructuring (variable names must be same as attribute names)
+// const { name, mainMenu, location } = restaurant
+const { name: restaurantName, mainMenu: menu, location: pta } = restaurant
+// console.log(restaurantName, menu, pta);
+
+const obj = { a: 23, b: 12, c: 9 };
+let a = 111;
+let b = 999;
+
+({ a, b } = obj);
+// {a, b} = obj // Error not work like this
+// console.log(a, b);
+
+const ali = {
+    info: {
+        name: "ali",
+        age: 20
+    },
+    location: {
+        street: "Abdullah",
+        city: "Sialkot"
+    }
+}
+
+const { info: { name } } = ali
+console.log(name);
+
+
+// const arr = [1, 2, 3]
+// const a = arr[0]
+// const b = arr[1]
+// const c = arr[2]
+
+// Destructuring
+// const [x, y, z] = arr
+
+// console.log(a, b, c);
+// console.log(x, y, z);
+
+// let [first, , second] = restaurant.categories
+// console.log(first, second);
+
+// [first, second] = [second, first]
+// console.log(first, second);
+
+// nested destructuring
+const nested = [1, 2, [3, 4]]
+// const [i, , j] = nested
+const [i, , [j]] = nested
+// console.log(i, j);
+
+// default values
+const [p = 1, q = 1, r = 1] = [5, 7]
+// console.log(p, q, r);
